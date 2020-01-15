757169447ccac56ada3d66c66ac3d7e86060d0ea
Kirill Uksusov <uksusoff@yandex.ru>
2020-01-15 11:58:38 +0400

    Version updated

--- src/Modules/ReminderModule.php
+++ src/Modules/ReminderModule.php
@@ -22,7 +22,7 @@ class ReminderModule extends AbstractModule implements ModuleCustomInterface, Mo
     use ModuleCustomTrait;
     use ModuleConfigTrait;
 
-    public const CUSTOM_VERSION = '2.0';
+    public const CUSTOM_VERSION = '2.0.2';
 
     public const CUSTOM_WEBSITE = 'https://github.com/UksusoFF/webtrees-reminder';
 
